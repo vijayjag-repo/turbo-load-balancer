114dfd0013ed623d73afebd0fa05936669e63866
Vijay Jagannathan <43071989+vijayjag-repo@users.noreply.github.com>
2023-12-28 22:42:27 +0530

    feat: add base skeleton which routes between different servers

--- src/main.go
+++ src/main.go
@@ -11,12 +11,24 @@ import (
 type Server interface {
 	IsAlive() bool
 	GetAddress() string
-	Serve(rw *http.ResponseWriter, r *http.Request)
+	Serve(rw http.ResponseWriter, r *http.Request)
 }
 
 type simpleServer struct {
-	addr  string
-	proxy *httputil.ReverseProxy
+	address string
+	proxy   *httputil.ReverseProxy
+}
+
+func (s *simpleServer) GetAddress() string {
+	return s.address
+}
+
+func (s *simpleServer) IsAlive() bool {
+	return true
+}
+
+func (s *simpleServer) Serve(rw http.ResponseWriter, r *http.Request) {
+	s.proxy.ServeHTTP(rw, r)
 }
 
 func createNewServer(address string) *simpleServer {
@@ -24,8 +36,8 @@ func createNewServer(address string) *simpleServer {
 	handleError(err)
 
 	return &simpleServer{
-		addr:  address,
-		proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+		address: address,
+		proxy:   httputil.NewSingleHostReverseProxy(serverUrl),
 	}
 }
 
@@ -48,4 +60,39 @@ func handleError(err error) {
 		fmt.Printf("Error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func (lb *loadBalancer) getNextAvailableServer() Server {
+	server := lb.servers[lb.roundRobinCount%len(lb.servers)]
+	for !server.IsAlive() {
+		lb.roundRobinCount++
+		server = lb.servers[lb.roundRobinCount%len(lb.servers)]
+	}
+	lb.roundRobinCount++
+	return server
+}
+
+func (lb *loadBalancer) serveProxy(rw http.ResponseWriter, r *http.Request) {
+	targetServer := lb.getNextAvailableServer()
+	fmt.Printf("Forwarding request to address: %q\n", targetServer.GetAddress())
+	targetServer.Serve(rw, r)
+}
+
+func main() {
+	// Create list of servers
+	servers := []Server{
+		createNewServer("http://www.google.com"),
+		createNewServer("http://www.duckduckgo.com"),
+		createNewServer("https://www.facebook.com"),
+	}
+
+	// Create new lb
+	lb := createNewLoadBalancer("8000", servers)
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		lb.serveProxy(rw, r)
+	})
+
+	fmt.Println("Serving requests at http://localhost:%s\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
 }
